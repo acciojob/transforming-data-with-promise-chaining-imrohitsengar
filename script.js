fca1d08fddccfcedc67350501c883acc2de8d4f9
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-10 23:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
 //your JS code here. If required.
-function hello(){
-	const inputVal=document.getElementById('ip').value;
-	return  new Promise((res)=>{
-		setTimeout(()=>{
-			res(inputVal);
-		},2000)
-	}).then((resolvedData)=>{
-		const output=document.getElementById('output');
-		output.textContent=`Result:${resolvedData}`;
-		return new Promise((res)=>{
-			setTimeout(()=>{
-			res(resolvedData);
-			},2000)
-		})
-	}).then((resolvedData)=>{
-		const output=document.getElementById('output');
-		output.textContent=`Result:${resolvedData * 2}`;
-		return new Promise((res)=>{
-			setTimeout(()=>{
-			res(resolvedData);
-			},2000)
-		})
-	}).then((resolvedData)=>{
-		const output=document.getElementById('output');
-		output.textContent=`Result:${(resolvedData*2)-3}`;
-		return new Promise((res)=>{
-			setTimeout(()=>{
-			res(resolvedData);
-			},2000)
-		})
-	}).then(resolvedData=>{
-		const output=document.getElementById('output');
-		output.textContent=`Result:${((resolvedData*2)-3)/2}`;
-		return new Promise((res)=>{
-			setTimeout(()=>{
-			res(resolvedData);
-			},2000)
-		})
-	}).then((resolvedData)=>{
-		const output=document.getElementById('output');
-		output.textContent=`Final Result:${(((resolvedData*2)-3)/2)+10}`;
-	})
-	
+function hello() {
+    const inputVal = Number(document.getElementById('ip').value);
+
+    return new Promise((res) => {
+        setTimeout(() => {
+            res(inputVal);
+        }, 2000);
+    })
+
+    .then((resolvedData) => {
+        document.getElementById('output').textContent =
+            `Result:${resolvedData}`;
+
+        return new Promise((res) => {
+            setTimeout(() => {
+                res(resolvedData * 2);
+            }, 2000);
+        });
+    })
+
+    .then((resolvedData) => {
+        document.getElementById('output').textContent =
+            `Result:${resolvedData}`;
+
+        return new Promise((res) => {
+            setTimeout(() => {
+                res(resolvedData - 3);
+            }, 2000);
+        });
+    })
+
+    .then((resolvedData) => {
+        document.getElementById('output').textContent =
+            `Result:${resolvedData}`;
+
+        return new Promise((res) => {
+            setTimeout(() => {
+                res(resolvedData / 2);
+            }, 2000);
+        });
+    })
+
+    .then((resolvedData) => {
+        document.getElementById('output').textContent =
+            `Result:${resolvedData}`;
+
+        return new Promise((res) => {
+            setTimeout(() => {
+                res(resolvedData + 10);
+            }, 2000);
+        });
+    })
+
+    .then((resolvedData) => {
+        document.getElementById('output').textContent =
+            `Final Result:${resolvedData}`;
+    });
 }
